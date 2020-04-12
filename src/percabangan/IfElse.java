61b21e7ab2d6825cea081b7161ccc085d719de6a
rizkaoktaviyani <rizkaoktaviyani10@gmail.com>
2020-04-12 15:47:40 +0800

    If dengan condition 1 false, condition true

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -7,8 +7,11 @@ public class IfElse {
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi jajan anda");
+        }else if(uangDiDompet>totalBelanja){
+            double angsul = uangDiDompet-totalBelanja;
+            System.out.println("Uang cukup, angsul : "+angsul);
         }else{
-            System.out.println("Uang cukup, selamat menikmati");
+            System.out.println("Uang pas.. UwU..");
         }
     }
 }
