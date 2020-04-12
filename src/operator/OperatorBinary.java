03a356e55350a863593e4b3cd00057be6bdbc4d1
rizkaoktaviyani <rizkaoktaviyani10@gmail.com>
2020-04-12 14:52:00 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -9,4 +9,13 @@ public class OperatorBinary {
     String nama = "Rizka Oktaviyani";
     int jumlahSks = 24;
     double indeksPrestasiKumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
