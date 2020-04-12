d259fcb2f0ec051f0a73e96dc8eaf27b49d9b4b5
rizkaoktaviyani <rizkaoktaviyani10@gmail.com>
2020-04-12 14:48:01 +0800

    Operator Binary Assigment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Rizka Oktaviyani";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+}
